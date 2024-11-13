d5e63e04de804916de95c6ccfd6558a17097e75f
Tyler <hughestyler2000@gmail.com>
2024-11-13 17:03:50 +0000

    files modifed     index.html - added input screeen                - added buttons                - added onclick values for buttons                - added classes for styling     index.js                - added functions for onclick     style.css                - added styling for html attributes

--- index.js
+++ index.js
@@ -2,3 +2,27 @@
  *  Author: Tyler Hughes
  */
 
+const screen = document.getElementById("screen");
+
+/**
+ *   appends value to screen
+ *   @param input - a string value of for a numeric operator or integer to be appended 
+ */ 
+function addToScreen(input){
+    screen.value += input;
+}
+
+// clears all values from screen
+function clearValues(){
+    screen.value="";
+}
+
+// evaluates code and displays the result
+function evaluateSum(){
+    try{
+        screen.value = eval(screen.value);
+    } 
+    catch(err){
+        screen.value = "Error";
+    }
+}
