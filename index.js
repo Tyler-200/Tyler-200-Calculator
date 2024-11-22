a7ca4f67283b1df7a99084557de3df477c8e83d0
Tyler-200 <hughestyler2000@gmail.com>
2024-11-22 16:57:10 +0000

    index.html and index.js modified: - added ans button to interface - added functionality for ans button

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 const inputScreen = document.getElementById("InputScreen");
 const calculationScreen = document.getElementById("CalculationScreen");
 
+var prevAns = "";
 /**
  *   appends value to screen
  *   @param input - a string value of for an integer to be appended to the calculator screen
@@ -13,6 +14,10 @@ function addToScreen(input){
     inputScreen.value += input;
 }
 
+/**
+ * removes value inputted into entry screen and adds it to exising sum with the selected operator
+ * @param input - a numeric operator to be applied to the previous number input
+ */
 function addOperator(input){
     calculationScreen.value += inputScreen.value;
     calculationScreen.value += input;
@@ -31,8 +36,18 @@ function evaluateSum(){
     inputScreen.value="";
     try{
         inputScreen.value = eval(calculationScreen.value);
+        prevAns = inputScreen.value;
     } 
     catch(err){
         inputScreen.value = "Error";
+    }
+}
+/**
+ * retreives previous result and displays it on screen
+ */
+function retreivePrevAns(){
+    if(prevAns != ""){
+        inputScreen.value = prevAns;
+    }else{
     }
 }
